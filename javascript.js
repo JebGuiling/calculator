22244e9b08652bf65ae9c88a0690a5138e2b135d
JebGuiling <cguiling26@gmail.com>
2023-09-25 20:11:49 +0800

    added win condition
    created a function that dedcides the win condition

--- javascript.js
+++ javascript.js
@@ -3,13 +3,13 @@ function getComputerChoice(){
     let num = Math.floor(Math.random() * 3 + 1);
 
     if (num == 1){
-        return "Rock";
+        return "rock";
 
     }
     else if (num == 2){
-        return "Paper";
+        return "paper";
     } else { 
-        return "Scissors";
+        return "scissors";
     }
 
 }
@@ -20,10 +20,35 @@ function getPlayerChoice(){
 }
 
 
-
+function game(playerSelection, computerChoice){
+    if (playerSelection == computerChoice){
+        return "Tie!";
+    }
+    else if (playerSelection == "rock" && computerChoice == "paper"){
+        return "Computer wins!";
+    }
+    else if (playerSelection == "rock" && computerChoice == "scissors"){
+        return "Player wins!";
+    }
+    else if (playerSelection == "paper" && computerChoice == "rock"){
+        return "Player wins!";
+    }
+    else if (playerSelection == "paper" && computerChoice == "scissors"){
+        return "Computer wins!";
+    }
+    else if (playerSelection == "scissors" && computerChoice == "rock"){
+        return "Computer wins!";
+    }
+    else if (playerSelection == "scissors" && computerChoice == "paper"){
+        return "Player wins!";
+    }
+}
 
 
 
 
 const playerSelection = getPlayerChoice();
 const computerChoice = getComputerChoice();
+
+
+console.log(game(playerSelection, computerChoice))
