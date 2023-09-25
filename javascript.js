dded8e8b20b2954c9c665d7e70e724871feca9b6
JebGuiling <5050832+JebGuiling@users.noreply.github.com.>
2023-09-25 20:32:56 +0800

    added a game loop
    included a for loop that loops that whole program 5 times

--- javascript.js
+++ javascript.js
@@ -1,54 +1,59 @@
 
-function getComputerChoice(){
-    let num = Math.floor(Math.random() * 3 + 1);
 
-    if (num == 1){
-        return "rock";
+for (let i = 0; i < 5; i++) {
+
+    function getComputerChoice(){
+        let num = Math.floor(Math.random() * 3 + 1);
+
+        if (num == 1){
+            return "rock";
+
+        }
+        else if (num == 2){
+            return "paper";
+        } else { 
+            return "scissors";
+        }
 
     }
-    else if (num == 2){
-        return "paper";
-    } else { 
-        return "scissors";
+
+
+
+
+    function getPlayerChoice(){
+        let choice = prompt("Input either Rock, Paper or Scissors").toLowerCase();
+        return choice;
     }
 
+
+    function playRound(playerSelection, computerChoice){
+        if (playerSelection == computerChoice){
+            return "Tie!";
+        }
+        else if (playerSelection == "rock" && computerChoice == "paper"){
+            return "Computer wins!";
+        }
+        else if (playerSelection == "rock" && computerChoice == "scissors"){
+            return "Player wins!";
+        }
+        else if (playerSelection == "paper" && computerChoice == "rock"){
+            return "Player wins!";
+        }
+        else if (playerSelection == "paper" && computerChoice == "scissors"){
+            return "Computer wins!";
+        }
+        else if (playerSelection == "scissors" && computerChoice == "rock"){
+            return "Computer wins!";
+        }
+        else if (playerSelection == "scissors" && computerChoice == "paper"){
+            return "Player wins!";
+        }
+    }
+
+
+    const playerSelection = getPlayerChoice();
+    const computerChoice = getComputerChoice();
+
+    console.log(computerChoice);
+    console.log(playRound(playerSelection, computerChoice));
 }
-
-function getPlayerChoice(){
-    let choice = prompt("Input either Rock, Paper or Scissors").toLowerCase();
-    return choice;
-}
-
-
-function game(playerSelection, computerChoice){
-    if (playerSelection == computerChoice){
-        return "Tie!";
-    }
-    else if (playerSelection == "rock" && computerChoice == "paper"){
-        return "Computer wins!";
-    }
-    else if (playerSelection == "rock" && computerChoice == "scissors"){
-        return "Player wins!";
-    }
-    else if (playerSelection == "paper" && computerChoice == "rock"){
-        return "Player wins!";
-    }
-    else if (playerSelection == "paper" && computerChoice == "scissors"){
-        return "Computer wins!";
-    }
-    else if (playerSelection == "scissors" && computerChoice == "rock"){
-        return "Computer wins!";
-    }
-    else if (playerSelection == "scissors" && computerChoice == "paper"){
-        return "Player wins!";
-    }
-}
-
-
-
-
-const playerSelection = getPlayerChoice();
-const computerChoice = getComputerChoice();
-
-
-console.log(game(playerSelection, computerChoice))
