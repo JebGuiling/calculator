aaf79f5953f9a3065370527919b9df2da9eb8521
JebGuiling <5050832+JebGuiling@users.noreply.github.com.>
2023-10-18 18:00:04 +0800

    Added a user interface
    added a user interface that uses Event listener

--- javascript.js
+++ javascript.js
@@ -2,68 +2,84 @@
 
 let playerScore = 0;
 let computerScore = 0;
+const score = document.querySelector('.score');
 
-for (let i = 0; i < 5; i++) {
+function getComputerChoice(){
+    let num = Math.floor(Math.random() * 3 + 1);
 
-    function getComputerChoice(){
-        let num = Math.floor(Math.random() * 3 + 1);
-
-        if (num == 1){
-            return "rock";
-
-        }
-        else if (num == 2){
-            return "paper";
-        } else { 
-            return "scissors";
-        }
+    if (num == 1){
+        return "rock";
 
     }
-
-
-
-
-    function getPlayerChoice(){
-        let choice = prompt("Input either Rock, Paper or Scissors").toLowerCase();
-        return choice;
+    else if (num == 2){
+        return "paper";
+    } else { 
+        return "scissors";
     }
 
-
-    function playRound(playerSelection, computerChoice){
-        if (playerSelection == computerChoice){
-            return "Tie!";
-        }
-        else if (playerSelection == "rock" && computerChoice == "paper"){
-            computerScore++
-            return "Computer wins!";
-        }
-        else if (playerSelection == "rock" && computerChoice == "scissors"){
-            playerScore++
-            return "Player wins!";
-        }
-        else if (playerSelection == "paper" && computerChoice == "rock"){
-            playerScore++
-            return "Player wins!";
-        }
-        else if (playerSelection == "paper" && computerChoice == "scissors"){
-            computerScore++
-            return "Computer wins!";
-        }
-        else if (playerSelection == "scissors" && computerChoice == "rock"){
-            computerScore++
-            return "Computer wins!";
-        }
-        else if (playerSelection == "scissors" && computerChoice == "paper"){
-            playerScore++
-            return "Player wins!";
-        }
-    }
-
-
-    const playerSelection = getPlayerChoice();
-    const computerChoice = getComputerChoice();
-
-    console.log(playRound(playerSelection, computerChoice));
-
-    console.log(`Score is Computer: ${computerScore} and Player: ${playerScore}`)
 }
+
+
+function playRound(playerSelection, computerChoice){
+    const wins = document.querySelector('.wins');
+
+    if (playerSelection == computerChoice){
+        wins.textContent = 'Tie!';
+    }
+    else if (playerSelection == "rock" && computerChoice == "paper"){
+        computerScore++
+        wins.textContent = 'Computer wins!';
+    }
+    else if (playerSelection == "rock" && computerChoice == "scissors"){
+        playerScore++
+        wins.textContent = 'Player wins!';
+    }
+    else if (playerSelection == "paper" && computerChoice == "rock"){
+        playerScore++
+        wins.textContent = 'Player wins!';
+    }
+    else if (playerSelection == "paper" && computerChoice == "scissors"){
+        computerScore++
+        wins.textContent = 'Computer wins!';
+    }
+    else if (playerSelection == "scissors" && computerChoice == "rock"){
+        computerScore++
+        wins.textContent = 'Computer wins!';
+    }
+    else if (playerSelection == "scissors" && computerChoice == "paper"){   
+        playerScore++
+        wins.textContent = 'Player wins!';
+        
+
+
+    }
+}
+let computerChoice = getComputerChoice();
+
+
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+
+
+rock.addEventListener('click', () => {
+    let playerSelection = 'rock';
+    console.log(playRound(playerSelection, computerChoice))
+    score.textContent = `Score is Computer: ${computerScore} and Player: ${playerScore}`
+
+})
+paper.addEventListener('click', () => {
+    let playerSelection = 'paper';
+    console.log(playRound(playerSelection, computerChoice))
+    score.textContent = `Score is Computer: ${computerScore} and Player: ${playerScore}`
+
+})
+scissors.addEventListener('click', () => {
+    let playerSelection =  'scissors';
+    console.log(playRound(playerSelection, computerChoice))
+    score.textContent = `Score is Computer: ${computerScore} and Player: ${playerScore}`
+
+})
+
+
+
